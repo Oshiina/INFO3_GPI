740744b75f05f291182d98b44e8f68169b45f93f
Oshiina <nicolaslabbe2000@gmail.com>
2019-05-14 17:25:37 +0200

    Commit #4 : Step0 C) Completed & tested
    Les neufs tests passent correctement

--- ricm3.gpi.given/Step0/warmup/layout/Component.java
+++ ricm3.gpi.given/Step0/warmup/layout/Component.java
@@ -10,147 +10,166 @@ package warmup.layout;
 
 public class Component {
 
-  protected Container m_parent;
-  protected int m_x, m_y, m_width, m_height;
+	protected Container m_parent;
+	protected int m_x, m_y, m_width, m_height;
 
-  public Component() {
-    this.m_parent = null;
-    this.m_x = 0;
-    this.m_y = 0;
-  }
+	public Component() {
+		this.m_parent = null;
+		this.m_width = 0;
+		this.m_height = 0;
+		this.m_x = 0;
+		this.m_y = 0;
+	}
 
-  public Component(Container parent) {
-    this.m_parent = parent;
-    this.m_x = 0;
-    this.m_y = 0;
-  }
+	public Component(Container parent) {
+		this.m_parent = parent;
+		this.m_parent.m_children.add(this);
+		this.m_width = 0;
+		this.m_height = 0;
+		this.m_x = parent.m_x;
+		this.m_y = parent.m_y;
+	}
 
-  public String toString() {
-    return getClass().getSimpleName() + "(" + m_x + "," + m_y + "," + m_width + "," + m_height + ")";
-  }
+	public String toString() {
+		return getClass().getSimpleName() + "(" + m_x + "," + m_y + "," + m_width + "," + m_height + ")";
+	}
 
-  /**
-   * Convert a global location to a location that is local to this component
-   * 
-   * @param l
-   */
-  public void toLocal(Location l) {
-    throw new Error("Not Yet Implemented");
-  }
+	/**
+	 * Convert a global location to a location that is local to this component
+	 * 
+	 * @param l
+	 */
+	public void toLocal(Location l) {
+		Component tmp = this;
+		while (tmp.m_parent != null) {
+			l.translate(-tmp.m_x, -tmp.m_y);
+			tmp = tmp.m_parent;
+		}
+	}
 
-  /**
-   * Convert a local location to a location that is global, that is, in the
-   * coordinates of the root of the layout tree.
-   * 
-   * @param l
-   */
-  public void toGlobal(Location l) {
-    throw new Error("Not Yet Implemented");
-  }
+	/**
+	 * Convert a local location to a location that is global, that is, in the
+	 * coordinates of the root of the layout tree.
+	 * 
+	 * @param l
+	 */
+	public void toGlobal(Location l) {
+		Component tmp = this;
+		while (tmp.m_parent != null) {
+			l.translate(tmp.m_x, tmp.m_y);
+			tmp = tmp.m_parent;
+		}
+	}
 
-  /**
-   * @return the parent container of this component
-   */
-  public Component parent() {
-    return m_parent;
-  }
+	/**
+	 * @return the parent container of this component
+	 */
+	public Component parent() {
+		return m_parent;
+	}
 
-  /**
-   * removes this component from its parent container.
-   * @throws IllegalStateException if this remove is not allowed.
-   */
-  public void remove() {
-    throw new Error("Not Yet Implemented");
-  }
+	/**
+	 * removes this component from its parent container.
+	 * @throws IllegalStateException if this remove is not allowed.
+	 */
+	public void remove() {
+		if (this.m_parent == null) {
+			throw new IllegalStateException();
+		}
+		this.m_parent.m_children.remove(this);
+	}
 
-  /**
-   * @return the location of this component in the coordinate system of its
-   *         parent.
-   */
-  public Location location() {
-    return new Location(m_x, m_y);
-  }
+	/**
+	 * @return the location of this component in the coordinate system of its
+	 *         parent.
+	 */
+	public Location location() {
+		return new Location(m_x, m_y);
+	}
 
-  /**
-   * Set the location of this component to the given location, location in the
-   * coordinate system of its parent.
-   * 
-   * @param loc
-   */
-  public void location(Location loc) {
-    m_x = loc.x();
-    m_y = loc.y();
-  }
+	/**
+	 * Set the location of this component to the given location, location in the
+	 * coordinate system of its parent.
+	 * 
+	 * @param loc
+	 */
+	public void location(Location loc) {
+		m_x = loc.x();
+		m_y = loc.y();
+	}
 
-  /**
-   * Set the location of this component to the given location, location in the
-   * coordinate system of its parent.
-   * 
-   * @param loc
-   */
-  public void setLocation(int x, int y) {
-    m_x = x;
-    m_y = y;
-  }
+	/**
+	 * Set the location of this component to the given location, location in the
+	 * coordinate system of its parent.
+	 * 
+	 * @param loc
+	 */
+	public void setLocation(int x, int y) {
+		m_x = x;
+		m_y = y;
+	}
 
-  public int x() {
-    return m_x;
-  }
+	public int x() {
+		return m_x;
+	}
 
-  public int y() {
-    return m_y;
-  }
+	public int y() {
+		return m_y;
+	}
 
-  public int width() {
-    return m_width;
-  }
+	public int width() {
+		return m_width;
+	}
 
-  public int height() {
-    return m_height;
-  }
+	public int height() {
+		return m_height;
+	}
 
-  /**
-   * Sets the dimension of this component
-   */
-  public void setDimension(int w, int h) {
-    m_width = w;
-    m_height = h;
-  }
+	/**
+	 * Sets the dimension of this component
+	 */
+	public void setDimension(int w, int h) {
+		m_width = w;
+		m_height = h;
+	}
 
-  /**
-   * Sets the bounds of this component.
-   * The given location is expressed in the
-   * coordinate system of its parent.
-   */
-  public void setBounds(int x, int y, int w, int h) {
-    m_x = x;
-    m_y = y;
-    m_width = w;
-    m_height = h;
-  }
+	/**
+	 * Sets the bounds of this component.
+	 * The given location is expressed in the
+	 * coordinate system of its parent.
+	 */
+	public void setBounds(int x, int y, int w, int h) {
+		m_x = x;
+		m_y = y;
+		m_width = w;
+		m_height = h;
+	}
 
-  /**
-   * Tells if the given location is within this component. 
-   * Nota bene: 
-   *    the location is given in the parent's coordinates.
-   * @param x
-   * @param y
-   * @return
-   */
-  public boolean inside(int x, int y) {
-    throw new Error("Not Yet Implemented");
-  }
+	/**
+	 * Tells if the given location is within this component. 
+	 * Nota bene: 
+	 *    the location is given in the parent's coordinates.
+	 * @param x
+	 * @param y
+	 * @return
+	 */
+	public boolean inside(int x, int y) {
+		return (x>=this.m_x && x<this.m_x+this.m_width && y>=this.m_y && y<this.m_y+this.m_height);
+	}
 
-  /**
-   * Select the component, on top, at the given location.
-   * Nota Bene: the location is given in local coordinates.
-   * @param x
-   * @param y
-   * @return this component only if it contains the given location, 
-   *         given in local coordinates
-   */
-  public Component select(int x, int y) {
-    throw new Error("Not Yet Implemented");
-  }
+	/**
+	 * Select the component, on top, at the given location.
+	 * Nota Bene: the location is given in local coordinates.
+	 * @param x
+	 * @param y
+	 * @return this component only if it contains the given location, 
+	 *         given in local coordinates
+	 */
+	public Component select(int x, int y) {
+		if (this.inside(x,y)){
+			return this;
+		}
+		return null;
+	}
 
 }
--- ricm3.gpi.given/Step0/warmup/layout/Container.java
+++ ricm3.gpi.given/Step0/warmup/layout/Container.java
@@ -1,5 +1,8 @@
 package warmup.layout;
 
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * This is a container within a tree of containers and components.
  * A container is a component that has children components.
@@ -11,40 +14,57 @@ package warmup.layout;
 
 public class Container extends Component {
 
-  Container() {
-    super();
-  }
+	protected List<Component> m_children;
 
-  public Container(Container parent) {
-    super(parent);
-  }
+	Container() {
+		super();
+		this.m_children = new LinkedList<Component>();
+	}
 
-  /**
-   * @return the number of components that are 
-   *         children to this container
-   */
-  public int childrenCount() {
-    throw new Error("Not Yet Implemented");
-  }
+	public Container(Container parent) {
+		super(parent);
+		this.m_children = new LinkedList<Component>();
+	}
 
-  /**
-   * @return the component indexed by the given
-   *         index.
-   */
-  public Component childrenAt(int i) {
-    throw new Error("Not Yet Implemented");
-  }
+	/**
+	 * @return the number of components that are 
+	 *         children to this container
+	 */
+	public int childrenCount() {
+		return this.m_children.size();
+	}
 
-  /**
-   * Select the component, on top, at the given location.
-   * The location is given in local coordinates.
-   * Reminder: children are above their parent.
-   * @param x
-   * @param y
-   * @return this selected component 
-   */
-  public Component select(int x, int y) {
-    throw new Error("Not Yet Implemented");
-  }
+	/**
+	 * @return the component indexed by the given
+	 *         index.
+	 */
+	public Component childrenAt(int i) {
+		return this.m_children.get(i);
+	}
+
+	/**
+	 * Select the component, on top, at the given location.
+	 * The location is given in local coordinates.
+	 * Reminder: children are above their parent.
+	 * @param x
+	 * @param y
+	 * @return this selected component 
+	 */
+	public Component select(int x, int y) {
+		if (this.inside(x, y)) {
+			int nb_children = this.childrenCount();
+			if (nb_children==0) {
+				return this;
+			}
+			for(int i=0;i<nb_children;i++) {
+				Component c = this.childrenAt(i);
+				if (c.inside(x, y)) {
+					return c.select(x,y);
+				}
+			}
+			return this;
+		}
+		return null;
+	}
 
 }
